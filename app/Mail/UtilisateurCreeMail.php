d69f8f2ee41fc26e016fdcf016239317a09b8419
abdelouafi <abdelouafioubenali.com>
2025-03-02 23:58:44 +0100

    envoiyer password et email apré lala creation de user

--- app/Mail/UtilisateurCreeMail.php
+++ app/Mail/UtilisateurCreeMail.php
@@ -10,17 +10,26 @@ class UtilisateurCreeMail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $utilisateur, $motDePasse;
+    public $users;
+    public $motDePasse;
 
+    /**
+     * Créer un nouvel e-mail.
+     */
     public function __construct($utilisateur, $motDePasse)
     {
-        $this->utilisateur = $utilisateur;
+        $this->users = $utilisateur;
         $this->motDePasse = $motDePasse;
     }
 
+    /**
+     * Construire l'e-mail.
+     */
     public function build()
     {
+        $user = $this->users;
+        $password = $this->motDePasse;
         return $this->subject('Bienvenue sur notre plateforme')
-                    ->view('emails.utilisateur_cree');
+        ->view('emails.utilisateur_cree',compact('user','password'));
     }
 }
