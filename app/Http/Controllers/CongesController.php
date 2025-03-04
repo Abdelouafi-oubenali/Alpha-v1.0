cb008bd0a93f84976d739a6ad1bcfd4a14bb1969
abdelouafi <abdelouafioubenali.com>
2025-03-04 14:30:12 +0000

    Travail sur la logique de demande de congé

--- app/Http/Controllers/CongesController.php
+++ app/Http/Controllers/CongesController.php
@@ -12,20 +12,55 @@ use Illuminate\Support\Facades\Auth;
 class CongesController extends Controller
 {
 
-    public function index()
-    {
-        $conges = Conges::all();
-        return view('conges.index',compact('conges'));
+    public function TotalJourConger (){
+        $employee = Auth::user();
+
+        $createdAt = $employee->created_at;
+        $today = Carbon::now();
+        $ToutalDay = $createdAt->diffInDays($today);
+    
+        $conges = null;
+    
+        if ($ToutalDay >= 366) {
+            $conges = 18;
+            
+            $BonusCoungre = floor($ToutalDay / 365) - 1; 
+            if ($BonusCoungre > 0) {
+                $conges += $BonusCoungre * 0.5;
+            }
+        } elseif ($ToutalDay < 366) {
+            $mois = $ToutalDay / 30;
+            $conges = round($mois * 1.5);
+        }
+
+        return $conges;
     }
 
+    public function index()
+{
+    $conges = $this->TotalJourConger();
+    return view('conges.index', compact('conges'));
+}
+
+
     public function create(){
-        return view('conges.create');
+        $conges = $this->TotalJourConger();
+        return view('conges.create',compact('conges'));
     }
 
     public function store(Request $request)
     {
-        
+        $conges = $this->TotalJourConger();
+
         $employee_id = Auth::id();
+        $date_debut = Carbon::parse($request->date_debut); 
+        $date_fin = Carbon::parse($request->date_fin); 
+        $check = $date_debut->diffInDays($date_fin);  
+         
+        if ($check > $conges) {
+            return redirect()->route('conges.create')  
+                ->withErrors(['message' => 'Impossible de demander ce congé, vous avez dépassé le nombre de jours disponibles.']);   
+        }
 
         // Créer le congé
         Conges::create([
@@ -40,3 +75,13 @@ class CongesController extends Controller
 
     }
 }
+
+
+
+
+
+
+
+
+
+
