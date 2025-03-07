44f08a3e1ae26e4a6ee2e5f982f568e26a42f321
abdelouafi <abdelouafioubenali.com>
2025-03-07 00:16:41 +0000

    Remplacer les routes par middleware

--- routes/web.php
+++ routes/web.php
@@ -12,28 +12,52 @@ use App\Http\Controllers\MailController;
 
 
 
+
 Route::view('/', 'welcome');
+
+
+Route::middleware('role:Admin|RH')->group(function(){
 Route::resource('departements', DepartmentController::class);
-Route::resource('users', UserController::class);
 Route::resource('formasion', FormationControler::class);
 Route::resource('posts',PostController::class);
 Route::resource('parcours',ParcourController::class);
+});
 
-Route::resource('conges', CongesController::class);
-Route::resource('mail', MailController::class);
+Route::middleware('role:Admin|RH|Manager')->group(function(){
+   Route::resource('mail', MailController::class);
+});
 
+// Route::middleware('role:Admin|RH|Manager')->group(function(){
+//     Route::resource('mail', MailController::class);
+//  });
+
+
+
+Route::middleware('role:Admin|RH|Manager')->group(function(){
+    Route::resource('users', UserController::class);
+    Route::put('/demandes/{id}/accepter', [MailController::class, 'accepter'])->name('demandes.accepter');
+    Route::put('/demandes/{id}/refuser', [MailController::class, 'refuser'])->name('demandes.refuser');
+    Route::get('/demandes/{id}/view', [MailController::class, 'view'])->name('demandes.view');
+});
+
+
+Route::middleware('role:Employe|Manager')->group(function(){
+    Route::resource('conges', CongesController::class);
 Route::view('/attonte', 'conges.attonte');
+
+});
 
 // Route::get('/conges/attonte', [CongesController::class, 'attonte'])->name('conges.attonte');
 // Route::get('/conges/{id}', [CongesController::class, 'show'])->name('conges.show');
 // Route::post('departements', [DepartmentController::class, 'store'])->name('departements.store');
-
 // Route::put('departments/{department}', [DepartmentController::class, 'update'])->name('departements.update');
+Route::get('/user/formation', [UserController::class, 'showFormations']);
+Route::post('/user/formation', [UserController::class, 'assignFormation'])->name('assignFormation'); //clack sur assign formasion
+
+Route::post('/user/formation/{formationId}', [UserController::class, 'assignFormation'])->name('assignFormation');
 
 
-Route::put('/demandes/{id}/accepter', [MailController::class, 'accepter'])->name('demandes.accepter');
-Route::put('/demandes/{id}/refuser', [MailController::class, 'refuser'])->name('demandes.refuser');
-Route::get('/demandes/{id}/view', [MailController::class, 'view'])->name('demandes.view');
+
 
 
 
